05054fa313424520960e3e1d642cfbcaa4a3698c
Lizzy Wiley <Lizzyrho@gmail.com>
2022-05-19 13:00:24 -0500

    done

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require("cors"); // cors instance
 // const PORT = process.env.PORT; // our port number for API calls
 const posts = require("./routes/PostRoute");
 
-app.use(cors({origin:'https://blogit-app.netlify.app'}));
+app.use(cors({origin:'https://628684f10b74fb0cb71b294f--blogit-app.netlify.app'}));
 
 const Post = require("./models/Post"); // manipulate our post entries 
 
